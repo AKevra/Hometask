b5bda54d31c83820d9efdf4b1ae4ffaec6eb10ea
AKevra <61617251+AKevra@users.noreply.github.com>
2020-04-01 17:32:15 +0300

    Add files via upload

--- LEVEL1/js/js.js
+++ LEVEL1/js/js.js
@@ -3,6 +3,8 @@ $(function() {
 
     slider.slick({
         infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
         slidesToShow: 1,
         slidesToScroll: 1,
         fade: true,
